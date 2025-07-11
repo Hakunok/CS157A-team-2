8539cfcc647865f9c0a87ee9b0c9b78ab2c8d304
aekn <aekn.admal@gmail.com>
2025-07-10 18:11:45 -0700

    updated schema to better align with professors requests

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,9 +1,14 @@
--- This doesn't drop the existing schema, you must do that beforehand if you choose to do so.
--- Or just use a different database name.
-
+/*
+ This doesn't drop the existing schema, you must do that beforehand if you choose to do so.
+ Or just use a different database name.
+ */
 CREATE DATABASE IF NOT EXISTS airchive;
 USE airchive;
 
+/*
+ Represents a user account on the platform
+ This table contains users of all roles: reader, author, admin
+ */
 CREATE TABLE IF NOT EXISTS user (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(20) NOT NULL UNIQUE,
@@ -17,6 +22,10 @@ CREATE TABLE IF NOT EXISTS user (
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+/*
+ Represents an author profile on the platform
+ An author entity can have an optional relationship (link) with a user entity whose role is "author"
+ */
 CREATE TABLE IF NOT EXISTS author (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT UNIQUE,
@@ -28,6 +37,9 @@ CREATE TABLE IF NOT EXISTS author (
     FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE SET NULL
 );
 
+/*
+ Represents a publication on the platform (paper, blog, article)
+ */
 CREATE TABLE IF NOT EXISTS publication (
     pub_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(150) NOT NULL,
@@ -45,12 +57,18 @@ CREATE TABLE IF NOT EXISTS publication (
     FOREIGN KEY (submitter_id) REFERENCES user(user_id) ON DELETE SET NULL
 );
 
+/*
+ Represents a topic on the platform (i.e., GenAI, Computer Vision, etc.)
+ */
 CREATE TABLE IF NOT EXISTS topic (
     topic_id INT PRIMARY KEY AUTO_INCREMENT,
     code VARCHAR(10) NOT NULL UNIQUE,
     name VARCHAR(50) NOT NULL UNIQUE
 );
 
+/*
+ Represents a user's (reader or author) reading list on the platform
+ */
 CREATE TABLE IF NOT EXISTS reading_list (
     list_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
@@ -61,6 +79,9 @@ CREATE TABLE IF NOT EXISTS reading_list (
     FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE
 );
 
+/*
+ Represents a user's (reader) request to become an author
+ */
 CREATE TABLE IF NOT EXISTS author_request (
     request_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
@@ -71,6 +92,11 @@ CREATE TABLE IF NOT EXISTS author_request (
     FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE
 );
 
+/*
+ Represents a many-to-many relationship between publication and author.
+ A publication entity can have 1 or more authors.
+ An author entity can have 1 or more publications.
+ */
 CREATE TABLE IF NOT EXISTS publication_author (
     pub_id INT,
     author_id INT,
@@ -80,6 +106,11 @@ CREATE TABLE IF NOT EXISTS publication_author (
     FOREIGN KEY (author_id) REFERENCES author(author_id) ON DELETE CASCADE
 );
 
+/*
+ Represents a many-to-many relationship between a publication and topic.
+ A publication entity can have 1 or more topics (i.e., Computer Vision & Biotech)
+ A topic entity can be related to 1 more publications.
+ */
 CREATE TABLE IF NOT EXISTS publication_topic (
     pub_id INT,
     topic_id INT,
@@ -88,6 +119,11 @@ CREATE TABLE IF NOT EXISTS publication_topic (
     FOREIGN KEY (topic_id) REFERENCES topic(topic_id) ON DELETE CASCADE
 );
 
+/*
+ Represents a many-to-many relationship between a user and a topic.
+ A user can interact with 1 or more topics.
+ A topic can be interacted with by 1 or more users.
+ */
 CREATE TABLE IF NOT EXISTS topic_interaction (
     interaction_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
@@ -98,6 +134,11 @@ CREATE TABLE IF NOT EXISTS topic_interaction (
     FOREIGN KEY (topic_id) REFERENCES topic(topic_id) ON DELETE CASCADE
 );
 
+/*
+ Represents a many-to-many relationship between a reading list and a publication.
+ A reading list entity can contain 1 or more publications.
+ A publication can be included within 1 or more reading lists.
+ */
 CREATE TABLE IF NOT EXISTS reading_list_item (
     list_id INT,
     pub_id INT,
@@ -108,6 +149,11 @@ CREATE TABLE IF NOT EXISTS reading_list_item (
     FOREIGN KEY (pub_id) REFERENCES publication(pub_id) ON DELETE CASCADE
 );
 
+/*
+ Represents a many-to-many relationship between a user and a publication.
+ A user entity can like 1 or more publications.
+ A publication entity can be liked by 1 or more users.
+ */
 CREATE TABLE IF NOT EXISTS likes (
     user_id INT,
     pub_id INT,

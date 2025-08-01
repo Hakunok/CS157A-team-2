014c6d3cdea0eae4afe337fdcf7e2b8b12e04587
Hakunok <102652109+Hakunok@users.noreply.github.com>
2025-07-31 18:22:52 -0700

    Create MiniCollection.java

---
+++ server/src/main/java/com/airchive/dto/MiniCollection.java
@@ -0,0 +1,24 @@
+package com.airchive.dto;
+
+import com.airchive.entity.Collection;
+import java.time.LocalDateTime;
+/**
+ * Lightweight representation of a {@link com.airchive.entity.Collection}, that will be used for
+ * bare minimum views in the frontend.
+ *
+ * <p>This record includes only the minimal metadata needed for display like title, default, and
+ * publicity.
+ *
+ * @param collectionId
+ * @param title
+ * @param isDefault
+ * @param isPublic
+ * @param createdAt
+ */
+public record MiniCollection(
+    int collectionId,
+    String title,
+    boolean isDefault,
+    boolean isPublic,
+    LocalDateTime createdAt
+) {}

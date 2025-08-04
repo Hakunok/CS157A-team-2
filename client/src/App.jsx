014c8834f7b05ce2cb0163a8045b7b7223eb8022
Hakunok <huytran8392@gmail.com>
2025-08-03 23:18:55 -0700

    update app and main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom"
-import AppRoutes from "@/routes/routes"
+import AppRoutes from "@/routes/AppRoutes"
 import Navbar from "@/components/layout/Navbar"
+import {Toaster} from "sonner";
 
 function App() {
   return (
       <BrowserRouter>
         <Navbar />
+        <Toaster richColors position="top-center" />
         <AppRoutes />
       </BrowserRouter>
   )
